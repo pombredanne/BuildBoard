7cb5cb8eb1dda53e529b3623511d5f45bc572889
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-10-25 14:00:33 +0300

    Add Closed Branches button

--- public/javascripts/app/navigation.js
+++ public/javascripts/app/navigation.js
@@ -1,9 +1,6 @@
 define(['jquery', 'underscore', 'app/jquery/jquery.navigation'], function ($, _, jn) {
     return {
-
-
         start: function (defaults) {
-            console.log(jn)
             $('a.navigation').navigation({
                 defaults: defaults,
                 activeClass: 'active',
@@ -16,18 +13,24 @@ define(['jquery', 'underscore', 'app/jquery/jquery.navigation'], function ($, _,
                     $roleBranches.addClass('hide-by-user');
                     $roleBranches.addClass('hide-by-entity');
 
+                    if (newValues.branches=='entity'){
+                        $('.role-branch[data-entity-id!=""]').removeClass('hide-by-entity');
+                    }
+                    else if (newValues.branches=='closed'){
+                        newValues.user = 'all';
+                        $('.role-branch[data-entity-isFinal]').removeClass('hide-by-entity');
+                    }
+                    else {
+                        $roleBranches.removeClass('hide-by-entity');
+                    }
+
                     if (newValues.user == 'all') {
                         $roleBranches.removeClass('hide-by-user');
                     } else {
                         $('.role-branch:has(.role-users[data-user-ids*="|' + value + '|"])').removeClass('hide-by-user');
                     }
 
-                    if (newValues.branches=='entity'){
-                        $('.role-branch[data-entity-id!=""]').removeClass('hide-by-entity');
-                    }
-                    else {
-                        $roleBranches.removeClass('hide-by-entity');
-                    }
+
 
                     $('.role-all-branches-count').text($('.role-branch:not(.hide-by-entity)').length);
                 }
